f8e0b580b68fcb6b43dff0c6a6dff460624b796e
Nutthapat Piasangka <my_nutthapat-26_@hotmail.com>
2020-05-17 05:02:49 +0700

    Updates

---
+++ precache-manifest.ba09d4e1c1fcd731eb204a0b96495098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2f6e44803f4998e121a97d85527c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f02a31fb92fe3c9d6bdd",
+    "url": "/static/css/main.55032f1e.chunk.css"
+  },
+  {
+    "revision": "80e0ed09cb83deea4b0e",
+    "url": "/static/js/2.a7320d53.chunk.js"
+  },
+  {
+    "revision": "f129a25a95b5779003d2444137eb2900",
+    "url": "/static/js/2.a7320d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02a31fb92fe3c9d6bdd",
+    "url": "/static/js/main.f5d5e372.chunk.js"
+  },
+  {
+    "revision": "517df531604185f7112d",
+    "url": "/static/js/runtime-main.45870b62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
